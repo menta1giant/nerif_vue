56f861a5ab094f0da40861633c7483bb81abc025
Mikhail Gostev <he.is.really.experienced@gmail.com>
2023-07-23 02:35:19 +0300

    fix

--- src/components/GlobalNotifications/NotificationMessage.vue
+++ src/components/GlobalNotifications/NotificationMessage.vue
@@ -29,5 +29,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/GlobalNotifications/global-notifications-message.scss';
+@import '@/assets/styles/GlobalNotifications/global-notification-message.scss';
 </style>
